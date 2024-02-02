0dc76a00b3170cf858f11f09b8abeab194f8ffc2
matheus <mathe@DESKTOP-D2RBRI4>
2024-02-01 22:18:58 -0300

    Add project files.

---
+++ src/CodeFlix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,50 @@
+﻿using CodeFlix.Catalog.Domain.Exceptions;
+
+namespace CodeFlix.Catalog.Domain.Entity
+{
+    public class Category
+    {
+        public Category(string name, string description, bool isActive = true)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            IsActive = isActive;
+            CreatedAt = DateTime.Now;
+
+            Validate();
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsActive { get; private set; }
+
+        private void Validate()
+        {
+            if(String.IsNullOrWhiteSpace(Name))
+                throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+            if (Name.Length < 3)
+                throw new EntityValidationException($"{nameof(Name)} should be at least 3 caracters long");
+            if (Name.Length > 255)
+                throw new EntityValidationException($"{nameof(Name)} should be less or equal 250 caracters long");
+            if (Description == null)
+                throw new EntityValidationException($"{nameof(Description)} should not be empty or null");
+            if (Description.Length > 10000)
+                throw new EntityValidationException($"{nameof(Description)} should be less or equal 10.000 caracters long");
+        }
+
+        public void Activate()
+        {
+            IsActive = true;
+            Validate();
+        }
+
+        public void Desativate()
+        {
+            IsActive = false;
+            Validate();
+        }
+    }
+}
---
+++ tests/CodeFlix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,172 @@
+﻿using CodeFlix.Catalog.Domain.Exceptions;
+using Xunit;
+using DomainEntity = CodeFlix.Catalog.Domain.Entity;
+namespace CodeFlix.Catalog.UnitTests.Domain.Entity.Category
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Instantiate()
+        {
+            //Arrange
+            var validDate = new 
+            { 
+                Name = "Category Name",
+                Description = "Category Description"
+            };
+            var datetimeBefore = DateTime.Now;
+
+            //Act
+            var category = new DomainEntity.Category(validDate.Name, validDate.Description);
+            var datetimeAfter = DateTime.Now;
+
+            //Assert
+            Assert.NotNull(category);
+            Assert.Equal(validDate.Name, category.Name);
+            Assert.Equal(validDate.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > datetimeBefore);
+            Assert.True(category.CreatedAt < datetimeAfter);
+            Assert.True(category.IsActive);
+        }
+
+        [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+        [Trait("Domain", "Category - Aggregates")]
+        [InlineData(true)]  
+        [InlineData(false)]  
+        public void InstantiateWithIsActive(bool isActive)
+        {
+            //Arrange
+            var validDate = new
+            {
+                Name = "Category Name",
+                Description = "Category Description"
+            };
+            var datetimeBefore = DateTime.Now;
+
+            //Act
+            var category = new DomainEntity.Category(validDate.Name, validDate.Description, isActive);
+            var datetimeAfter = DateTime.Now;
+
+            //Assert
+            Assert.NotNull(category);
+            Assert.Equal(validDate.Name, category.Name);
+            Assert.Equal(validDate.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > datetimeBefore);
+            Assert.True(category.CreatedAt < datetimeAfter);
+            Assert.Equal(isActive, category.IsActive);
+        }
+
+        [Theory(DisplayName = nameof(InstantiateErrorWhenNameIsEmpty))]
+        [Trait("Domain", "Category - Aggregates")]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("   ")]
+        public void InstantiateErrorWhenNameIsEmpty(string? name)
+        {
+            Action action = 
+                () => new DomainEntity.Category(name!, "Category Description");
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+
+            Assert.Equal("Name should not be empty or null", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(InstantiateErrorWhenDescriptionIsNull))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void InstantiateErrorWhenDescriptionIsNull()
+        {
+            Action action =
+                () => new DomainEntity.Category("Category name", null!);
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+
+            Assert.Equal("Description should not be empty or null", exception.Message);
+        }
+
+        [Theory(DisplayName = nameof(InstantiateErrorWhenNameIsLessThan3Caracters))]
+        [Trait("Domain", "Category - Aggregates")]
+        [InlineData("AB")]
+        [InlineData("A")]
+        [InlineData("A1")]
+        public void InstantiateErrorWhenNameIsLessThan3Caracters(string invalidName)
+        {
+            Action action =
+                () => new DomainEntity.Category(invalidName, "Category Description"!);
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+
+            Assert.Equal("Name should be at least 3 caracters long", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(InstantiateErrorWhenNameIsGreaterThan250Caracters))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void InstantiateErrorWhenNameIsGreaterThan250Caracters()
+        {
+            var invalidName = String.Join(null, Enumerable.Range(1, 256).Select(_ => "a").ToArray());
+
+            Action action =
+                () => new DomainEntity.Category(invalidName, "Category Description"!);
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+
+            Assert.Equal("Name should be less or equal 250 caracters long", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(InstantiateErrorWhenNameIsGreaterThan10_000Caracters))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void InstantiateErrorWhenNameIsGreaterThan10_000Caracters()
+        {
+            var invalidDescription = String.Join(null, Enumerable.Range(1, 10001).Select(_ => "a").ToArray());
+
+            Action action =
+                () => new DomainEntity.Category("Category Name", invalidDescription!);
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+
+            Assert.Equal("Description should be less or equal 10.000 caracters long", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(Activate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Activate()
+        {
+            //Arrange
+            var validDate = new
+            {
+                Name = "Category Name",
+                Description = "Category Description"
+            };
+
+            //Act
+            var category = new DomainEntity.Category(validDate.Name, validDate.Description, false);
+            category.Activate();
+
+            //Assert
+            Assert.True(category.IsActive);
+        }
+
+        [Fact(DisplayName = nameof(Desativate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Desativate()
+        {
+            //Arrange
+            var validDate = new
+            {
+                Name = "Category Name",
+                Description = "Category Description"
+            };
+
+            //Act
+            var category = new DomainEntity.Category(validDate.Name, validDate.Description, true);
+            category.Desativate();
+
+            //Assert
+            Assert.False(category.IsActive);
+        }
+    }
+}
